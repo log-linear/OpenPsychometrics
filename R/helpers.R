bd34d064380a4f68b2ce7982420751c95aa6cc8f
Victor Faner <victor@faner.co>
2020-06-17 17:49:07 -0500

    Working Big 5 score calculator

--- R/helpers.R
+++ R/helpers.R
@@ -1,36 +1,56 @@
 #===============================================================================
 # author: Victor Faner
+# description: Miscellaneous helper functions for analyzing datasets
 #===============================================================================
 
-calc_big_five <- function(raw) {
-  # library(tidyverse)
-  # library(here)
-  # 
-  # long <- raw %>% 
-  #   pivot_longer(cols = matches("\\w\\d+"),
-  #                names_to = c("question_type", "question_number"),
-  #                names_pattern = "(\\w)(\\d+)") %>% 
-  #   mutate(question_number = as.numeric(question_number))
-  # 
-  # big_5 <- long %>% 
-  #   filter(question_type != "R") %>% 
-  #   rename(trait = question_type) %>% 
-  #   
-  #   # Join in Big 5 scoring guide
-  #   left_join(  
-  #     read_csv(here("etc/big_five_scoring.csv")) %>% 
-  #       
-  #       # Get question numbers by trait to match dataset numbering
-  #       group_by(trait) %>% 
-  #       mutate(question_number = row_number()) %>%
-  #       ungroup()
-  #   ) %>%
-  #   
-  #   # Convert item-level response values according to scoring guide
-  #   mutate(value = value * if_else(scoring_operation == "+", 1, -1)) %>%
-  #   
-  #   # Calculate scores for each Big 5 trait
-  #   group_by(id, trait) %>% 
-  #   mutate(trait_score = base_score + sum(value)) %>% 
-  #   ungroup()
+calc_big_five <- function(dataset) {
+  load(here::here("R/sysdata.rda"))
+  
+  big_5_scores <- dataset %>%
+    
+    # Create a unique ID for each record in the dataset
+    dplyr::mutate(id = dplyr::row_number()) %>% 
+    
+    # Pivot into a long format
+    tidyr::pivot_longer(cols = tidyselect::matches("\\w\\d+"),
+                        names_to = c("trait", "question_number"),
+                        names_pattern = "(\\w)(\\d+)") %>%
+    dplyr::mutate(question_number = as.numeric(question_number)) %>%
+    dplyr::filter(
+      
+      # Filter down to Big Five traits
+      (  trait == "O"
+       | trait == "C"
+       | trait == "E"
+       | trait == "A"
+       | trait == "N")
+      &
+      # Account for datasets with additional, non-Big Five questions
+      (  value == 1
+       | value == 2
+       | value == 3
+       | value == 4
+       | value == 5)
+    ) %>%
+    
+    # Join in Big 5 scoring guide
+    left_join(
+      big_5_scoring_guide
+    ) %>%
+
+    # Convert item-level response values according to scoring guide
+    dplyr::mutate(value = value * ifelse(scoring_operation == "+", 1, -1)) %>%
+
+    # Calculate scores for each Big 5 trait
+    dplyr::group_by(id, trait) %>%
+    dplyr::mutate(trait_score = base_score + sum(value)) %>%
+    dplyr::ungroup() %>% 
+    dplyr::select(-id, 
+                  -question_number, 
+                  -scoring_operation, 
+                  -value, 
+                  -base_score) %>% 
+    dplyr::distinct()
+  
+  return(big_5_scores)
 }
