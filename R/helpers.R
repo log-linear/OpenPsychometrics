84f2d12323f4f240b40d74225c2d82c57d70e880
Victor Faner <victor@faner.co>
2020-06-18 11:58:27 -0500

    Fix Big 5 scoring function
    - Old logic would omit Big 5 item responses of 0, which are
    present in certain datasets. Switching to full join fixes this
    - Simplify/clean up code style

--- R/helpers.R
+++ R/helpers.R
@@ -3,57 +3,61 @@
 # description: Miscellaneous helper functions for analyzing datasets
 #===============================================================================
 
-calc_big_five <- function(dataset) {
+calc_big_5 <- function(dataset) {
+  # Load scoring guide
   load(here::here("R/sysdata.rda"))
   
+  # Create a locally-scoped row ID column
+  dataset <- dataset %>%  
+    dplyr::mutate(`_id` = dplyr::row_number())
+  
+  # Calculate Big 5 trait scores
   big_5_scores <- dataset %>%
     
-    # Create a locally-scoped variable to uniquely ID each row
-    dplyr::mutate(`_id` = dplyr::row_number()) %>% 
-    
-    # Pivot into a long format
+    # Reshape into a long format
     tidyr::pivot_longer(cols = tidyselect::matches("\\w\\d+"),
                         names_to = c("trait", "question_number"),
                         names_pattern = "(\\w)(\\d+)") %>%
     dplyr::mutate(question_number = as.numeric(question_number)) %>%
     dplyr::filter(
       
-      # Filter down to Big Five traits
-      (  trait == "O"
-       | trait == "C"
-       | trait == "E"
-       | trait == "A"
-       | trait == "N")
+      # Filter down to Big Five questions
+      (   trait == "O"
+        | trait == "C"
+        | trait == "E"
+        | trait == "A"
+        | trait == "N" )
       &
-      # Account for datasets with non-Big Five responses
-      (  value == 1
-       | value == 2
-       | value == 3
-       | value == 4
-       | value == 5)
+      # Account for datasets with non-Big Five responses. Note that some
+      # datasets have response values of 0. These records will return NA for
+      # their Big 5 trait scores
+      (   value == 1
+        | value == 2
+        | value == 3
+        | value == 4
+        | value == 5 )
     ) %>%
     
     # Join in Big 5 scoring guide
-    dplyr::left_join(
-      big_5_scoring_guide
-    ) %>%
+    dplyr::left_join(big_5_scoring_guide) %>%
 
     # Convert item-level response values according to scoring guide
     dplyr::mutate(value = value * ifelse(scoring_operation == "+", 1, -1)) %>%
 
     # Calculate scores for each Big 5 trait
-    dplyr::group_by(`_id`, trait) %>%
-    dplyr::mutate(trait_score = base_score + sum(value)) %>%
+    dplyr::group_by(`_id`, trait, base_score) %>%
+    dplyr::summarize(trait_score = sum(value)) %>%
     dplyr::ungroup() %>% 
-    dplyr::select(-question_number, 
-                  -scoring_operation, 
-                  -value, 
-                  -base_score) %>% 
-    dplyr::distinct() %>% 
+    dplyr::mutate(trait_score = trait_score + base_score) %>% 
+    dplyr::select(-base_score) %>% 
+    
+    # Reshape back to original format
     tidyr::pivot_wider(names_from = trait, 
                        values_from = trait_score) %>% 
-    dplyr::select(-`_id`) %>% 
-    dplyr::left_join(dataset)
+    
+    # Join Big 5 scores back onto original dataset
+    dplyr::full_join(dataset) %>% 
+    dplyr::select(-`_id`)
   
   return(big_5_scores)
 }
